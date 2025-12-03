8244cc11ccbb20b55067dec9b7eef2c706e2b780
LiamAllbee <yoshilink1@gmail.com>
2025-12-02 21:21:51 -0500

    Set up routing so links on left side of the page return their respective categories.

--- 03-frontend/angular-ecommerce/src/app/app-module.ts
+++ 03-frontend/angular-ecommerce/src/app/app-module.ts
@@ -7,12 +7,21 @@ import { ProductList } from './components/product-list/product-list';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ProductService } from './services/product-service';
 
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  {path: 'category/:id', component: ProductList},
+  {path: 'category', component: ProductList},
+  {path: 'products', component: ProductList},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+];
 @NgModule({
   declarations: [
     App,
     ProductList
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule
   ],
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product-service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: false,
   templateUrl: './product-list-grid.html',
-//templateUrl: './product-list-table.html',
-//templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList {
 
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  currentCategoryId: number = 1;
+
+  constructor(private productService: ProductService, 
+              private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    
+    if(hasCategoryId) {
+      // get the "id" parameter string. concert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      // if category id is not available default to category 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
